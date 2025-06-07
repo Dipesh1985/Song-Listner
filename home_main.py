332173aef70a3fd53577a4578789bdc16a421b8d
Dipesh Mani Tiwari <tiwaridipesh739@gmail.com>
2025-06-07 22:37:12 +0530

    Update home_main.py

--- home_main.py
+++ home_main.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.header("Listen Song")
 st.subheader("  ",divider=True)
 st.image("https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/07/just-listen-to-the-song.jpg?q=50&fit=crop&w=1140&h=&dpr=1.5")
-a=st.file_uploader("Select a music File",type=["mp3"])
+a=st.file_uploader("Select a music File",type=None)
 
 if a:
     try:
